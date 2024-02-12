8413acbce0c6be26f54d05f244a63cb9c7ff31c3
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-12 22:22:08 +0200

    feat: add server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("it work");
+import * as path from "path";
+import { createServer as createServerHttp, Server } from "http";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const __dirname = path.dirname(__filename);
+
+
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = parseInt(process.env.PORT || '3000');
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});

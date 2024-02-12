0758ef91ab27929388a3f8fa058153af34adff5a
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-12 22:39:05 +0200

    feat: separate method of server create

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
-import * as path from "path";
-import { createServer as createServerHttp, Server } from "http";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import App from "./App";
 
 dotenv.config();
 
-const __dirname = path.dirname(__filename);
+const PORT = 3000;
+const BASE_URL = `http://localhost:${PORT}`;
 
+export const app = new App();
 
-console.log(`Path segment separator is "${path.sep}"`);
-
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
-
-const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
-});
-
-const PORT = parseInt(process.env.PORT || '3000');
-
-myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
+  try {
+    app.listen(+PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }

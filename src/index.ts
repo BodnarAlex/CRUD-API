a59d04f7ee79192f734a8b905022d6f993b51c75
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-10-26 09:46:07 +0200

    feat: add multi command

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import { DEFAULT_PORT } from "./common/constant";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
 
 app.use(express.json());
 app.use("/api", userRoutes);

6f522a8da4971d716333e9c1f70b5d5badd95544
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-10-26 07:59:12 +0200

    feat: add user types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import * as dotenv from "dotenv";
 import App from "./App";
+import { DEFAULT_PORT } from "./common/constant";
 
 dotenv.config();
 
 export const app = new App();
 
 const { envArg } = process.env;
-const PORT = parseInt(envArg, 10) || 3000;
+const PORT = parseInt(envArg, 10) || DEFAULT_PORT;
 
 try {
   app.listen(+PORT, () => {
